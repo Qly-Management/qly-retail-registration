5e0136c458850e394f103a056f5535c634e8d24d
Ansh Godha <ag759@cornell.edu>
2020-05-21 15:37:39 +0530

    just a commit to save all the bootstrap stuff, which ma ny be deleted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import logo from './assets/Logo.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import StateSelect from './StateSelect'
+import States from './states.js'
 
 function App() {
+
+  const [state, setState] = useState('Select State')
+  const [cheapness, setCheapness] = useState(1)
+
+  function stateSelect(e)  {
+    setState(e.target.value)
+  }
+
+  function stateSelect(e)  {
+    setCheapness(e.target.value)
+  }
+
   return (
     <div className="App">
       <img className="logo" src={logo} alt="Qly logo" />
@@ -15,8 +27,33 @@ function App() {
         <input type="text" placeholder="Address line 1" />
         <input type="text" placeholder="Address line 2" />
         <input type="text" placeholder="City" />
-        <StateSelect />
+        {/* <StateSelect className="ddown" style={{ backgroundColor: 'blue'}}/> */}
+        
+        <select className="stateSelect" name="select" onChange={stateSelect}>
+        {
+          ['Select State', ...Object.values(States)].map(function(n) { 
+            return (<option value={n} selected={state === n}>{n}</option>);
+          })
+        }
+        </select>
+        <input type="text" placeholder="Pin Code" />
 
+        <select className="stateSelect" name="select" onChange={setCheapness}>
+        {
+          ['Cheapness', 1,2,3,4].map(function(n) { 
+            return (<option value={n} selected={state === n}>{n}</option>);
+          })
+        }
+        </select>
+
+        <input type="text" placeholder="Total Store Capacity" />
+        <input type="text" placeholder="Capacity Cushion" />
+
+
+
+
+        
+                
       </form>
 
     </div>
