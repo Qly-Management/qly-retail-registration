125632a3ca3dc90df551702f20df18cd7a4cc1e2
Ansh Godha <ag759@cornell.edu>
2020-05-22 01:15:20 +0530

    basic form done w validation

--- src/Form.jsx
+++ src/Form.jsx
@@ -7,9 +7,11 @@ export default function () {
 
   // States for error messages
   const [nameError, setNameError] = useState("");
-  const [address, setAddressError] = useState("");
+  const [addressError, setAddressError] = useState("");
   const [cityError, setCityError] = useState("");
-  const [numberError, setNumberError] = useState("");
+  const [pinError, setPinError] = useState("");
+  const [capacityError, setCapacityError] = useState("");
+  const [cushionError, setCushionError] = useState("");
   const [cushCapError, setCushCapError] = useState("");
   const [timeError, setTimeError] = useState("");
   const [stateError, setStateError] = useState("");
@@ -32,6 +34,115 @@ export default function () {
   const [cheapness, setCheapness] = useState("Cheapness");
   const [checklist, setChecklist] = useState([]);
 
+  // input validation function
+  function validate(field, value) {
+    // validate company name
+    switch (field) {
+      case "name":
+        if (!value.trim()) {
+          setNameError("Please enter a valid company name");
+          return false;
+        } else {
+          setNameError("");
+          return true;
+        }
+
+      case "address":
+        if (!value.trim()) {
+          setAddressError("Please enter a valid address value");
+          return false;
+        } else {
+          setAddressError("");
+          return true;
+        }
+
+      case "city":
+        if (!value.trim()) {
+          setCityError("Please enter a valid city name");
+          return false;
+        } else {
+          setCityError("");
+          return true;
+        }
+
+      case "pin":
+        if (isNaN(value) || !value.trim()) {
+          setPinError("The PIN code needs to be an integer.");
+          return false;
+        } else {
+          setPinError("");
+          return true;
+        }
+
+      case "capacity":
+        if (isNaN(value) || !value.trim()) {
+          setCapacityError("The capacity needs to be an integer.");
+          return false;
+        } else {
+          setCapacityError("");
+          return true;
+        }
+
+      case "cushion":
+        if (isNaN(value) || !value.trim()) {
+          setCushionError("The cushion needs to be an integer.");
+          return false;
+        } else {
+          setCushionError("");
+          return true;
+        }
+
+      case "state":
+        if (value === "Select State") {
+          setStateError("Please select a state");
+          return false;
+        } else {
+          setStateError("");
+          return true;
+        }
+
+      case "cheapness":
+        if (value === "Cheapness") {
+          setCheapError("Please select a cheapness level");
+          return false;
+        } else {
+          setCheapError("");
+          return true;
+        }
+
+      case "time":
+        if (!value) {
+          setTimeError("Please select a valid time");
+          return false;
+        } else {
+          setTimeError("");
+          return true;
+        }
+
+      case "timeCheck":
+        if (value.length === 0) {
+          setChecklistError("Please select at least one time interval");
+          return false;
+        } else {
+          setChecklistError("");
+          return true;
+        }
+
+      default:
+        return false;
+    }
+  }
+
+  function validateCushCap(cush, cap) {
+    if (cush && cap && Number.parseInt(cush) > Number.parseInt(cap)) {
+      setCushCapError("The cushion cannot be greater than the capacity!");
+      return false;
+    } else {
+      setCushCapError("");
+      return true;
+    }
+  }
+
   // States to handle opening and closing of time selection fields
   const [timePickerOpen, setPickerOpen] = useState(false);
   const [timePickerOpen2, setPickerOpen2] = useState(false);
@@ -50,7 +161,10 @@ export default function () {
       <form action="" method="post">
         <input
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {
+            setName(e.target.value);
+            validate("name", e.target.value);
+          }}
           type="text"
           placeholder="Enter company name"
         />
@@ -59,10 +173,15 @@ export default function () {
 
         <input
           value={address1}
-          onChange={(e) => setAddress1(e.target.value)}
+          onChange={(e) => {
+            setAddress1(e.target.value);
+            validate("address", e.target.value);
+          }}
           type="text"
           placeholder="Address line 1"
         />
+
+        <div className="error">{addressError}</div>
 
         <input
           value={address2}
@@ -73,16 +192,24 @@ export default function () {
 
         <input
           value={city}
-          onChange={(e) => setCity(e.target.value)}
+          onChange={(e) => {
+            setCity(e.target.value);
+            validate("city", e.target.value);
+          }}
           type="text"
           placeholder="City"
         />
+
+        <div className="error">{cityError}</div>
 
         <select
           style={{ color: state === "Select State" ? "#A9A9A9" : "black" }}
           className="stateSelect"
           name="select"
-          onChange={(e) => setState(e.target.value)}
+          onChange={(e) => {
+            setState(e.target.value);
+            validate("state", e.target.value);
+          }}
         >
           {["Select State", ...Object.values(States)].map(function (n) {
             return (
@@ -93,18 +220,28 @@ export default function () {
           })}
         </select>
 
+        <div className="error">{stateError}</div>
+
         <input
           value={pin}
-          onChange={(e) => setPin(e.target.value)}
+          onChange={(e) => {
+            setPin(e.target.value);
+            validate("number", e.target.value);
+          }}
           type="text"
           placeholder="Pin Code"
         />
+
+        <div className="error">{pinError}</div>
 
         <select
           style={{ color: cheapness === "Cheapness" ? "#A9A9A9" : "black" }}
           className="stateSelect"
           name="select"
-          onChange={(e) => setCheapness(e.target.value)}
+          onChange={(e) => {
+            setCheapness(e.target.value);
+            validate("cheapness", e.target.value);
+          }}
         >
           {["Cheapness", 1, 2, 3, 4].map(function (n) {
             return (
@@ -115,19 +252,35 @@ export default function () {
           })}
         </select>
 
+        <div className="error">{cheapError}</div>
+
         <input
           value={capacity}
-          onChange={(e) => setCapacity(e.target.value)}
+          onChange={(e) => {
+            setCapacity(e.target.value);
+            validate("capacity", e.target.value);
+            validateCushCap(cushion, e.target.value);
+          }}
           type="text"
           placeholder="Total Store Capacity"
         />
 
+        <div className="error">{capacityError}</div>
+
         <input
           value={cushion}
-          onChange={(e) => setCushion(e.target.value)}
+          onChange={(e) => {
+            setCushion(e.target.value);
+            validate("cushion", e.target.value);
+            validateCushCap(e.target.value, capacity);
+          }}
           type="text"
           placeholder="Capacity Cushion"
         />
+
+        <div className="error">{cushionError}</div>
+        <div className="error">{cushCapError}</div>
+        <div style={{ height: 10 }}></div>
 
         <TimePicker
           use12Hours
@@ -136,10 +289,14 @@ export default function () {
           onOpenChange={handleOpenChange}
           format="HH:mm"
           placeholder="Enter opening time"
-          onChange={(_, timeString) => setOpeningTime(timeString)}
+          onChange={(_, timeString) => {
+            setOpeningTime(timeString);
+            validate("time", timeString);
+          }}
         />
         <br />
 
+        <div className="error">{timeError}</div>
         <div style={{ marginBottom: 10 }} />
 
         <TimePicker
@@ -149,9 +306,13 @@ export default function () {
           onOpenChange={handleOpenChange2}
           format="HH:mm"
           placeholder="Enter closing time"
-          onChange={(_, timeString) => setClosingTime(timeString)}
+          onChange={(_, timeString) => {
+            setClosingTime(timeString);
+            validate("time", timeString);
+          }}
         />
 
+        <div className="error">{timeError}</div>
         <div style={{ marginBottom: 10 }} />
 
         <input
@@ -169,10 +330,17 @@ export default function () {
         />
 
         <Checkbox.Group
+          style={{ marginTop: 10 }}
           options={timeIntervalOptions}
           value={checklist}
-          onChange={(checkList) => setChecklist(checkList)}
+          onChange={(checkList) => {
+            console.log(checkList);
+            validate("timeCheck", checkList);
+            setChecklist(checkList);
+          }}
         />
+
+        <div className="error">{checklistError}</div>
 
         <button name="submit" value="submit" className="submit" type="submit">
           Submit
