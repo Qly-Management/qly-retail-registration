458b845e561e435b57def086eba4d7fa4c60cad8
Ansh Godha <ag759@cornell.edu>
2020-05-21 17:19:47 +0530

    initial version done, design changes and API integration to follow

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import './App.css'
 import logo from './assets/Logo.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import States from './states.js'
+import { TimePicker, Checkbox } from 'antd'
+
 
 function App() {
 
+  const timeIntervalOptions = ['15', '30', '60']
+
   const [state, setState] = useState('Select State')
   const [cheapness, setCheapness] = useState(1)
+  const [timePickerOpen, setPickerOpen] = useState(false)
+  const [timePickerOpen2, setPickerOpen2] = useState(false)
+  const [checklist, setChecklist] = useState([])
+
+  function handleOpenChange(open) {
+    setPickerOpen(open)
+  }
+
+  function handleOpenChange2(open) {
+    setPickerOpen2(open)
+  }
 
   function stateSelect(e)  {
     setState(e.target.value)
   }
 
-  function stateSelect(e)  {
+  function cheapSelect(e)  {
     setCheapness(e.target.value)
   }
 
@@ -24,9 +38,13 @@ function App() {
 
       <form>
         <input type="text" placeholder="Enter company name" />
+
         <input type="text" placeholder="Address line 1" />
+
         <input type="text" placeholder="Address line 2" />
+
         <input type="text" placeholder="City" />
+
         {/* <StateSelect className="ddown" style={{ backgroundColor: 'blue'}}/> */}
         
         <select className="stateSelect" name="select" onChange={stateSelect}>
@@ -36,25 +54,58 @@ function App() {
           })
         }
         </select>
+        
         <input type="text" placeholder="Pin Code" />
 
-        <select className="stateSelect" name="select" onChange={setCheapness}>
+        <select className="stateSelect" name="select" onChange={cheapSelect}>
         {
           ['Cheapness', 1,2,3,4].map(function(n) { 
-            return (<option value={n} selected={state === n}>{n}</option>);
+            return (<option value={n} selected={cheapness === n}>{n}</option>);
           })
         }
         </select>
 
         <input type="text" placeholder="Total Store Capacity" />
-        <input type="text" placeholder="Capacity Cushion" />
 
+        <input type="text" placeholder="Capacity Cushion"/>    
 
+        <TimePicker
+          use12Hours
+          className="tp"
+          open={timePickerOpen}
+          onOpenChange={handleOpenChange}
+          format="HH:mm"
+          placeholder="Enter opening time"
+        />
+        <br />
 
+        <div style={{marginBottom:10}}/>
 
-        
-                
+        <TimePicker
+          use12Hours
+          className="tp"
+          open={timePickerOpen2}
+          onOpenChange={handleOpenChange2}
+          format="HH:mm"
+          placeholder="Enter closing time"
+        />
+
+        <div style={{marginBottom:10}}/>
+
+        <input type="text" placeholder="Special Instructions"/>
+
+        <input type="text" placeholder="Description"/>
+
+        <Checkbox.Group
+          options={timeIntervalOptions}
+          value={checklist}
+          onChange={(checkList) => setChecklist(checkList)}
+        />
+
+        <button className="submit" type="submit">Submit</button>
+       
       </form>
+       
 
     </div>
   );
---
+++ src/CustomInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function({style, type, placeholder, label}) {
+  return (
+    <div className="customInput" style={{...style, backgroundColor: 'red'}}>
+      <input style={{width: '100%'}} type={type} placeholder={placeholder} />
+      <p style={{fontSize: 10}}>{label}</p>
+    </div>
+  )
+}
